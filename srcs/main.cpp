a48d1ffeb8a3c1b2972fc7824438ffaba409b81f
Iipal <malkoleyplay@gmail.com>
2019-07-09 17:07:15 +0300

    reader

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
-using std::cout;
-using std::endl;
+int main(int argc, char *argv[]) {
+    --argc; ++argv;
+    bool    start_work = false;
 
-int main(void) {
-    cout << "Hello, World!" << endl;
+    std::vector<std::string> _commandQueue;
+    if (!argc) {
+        std::string _tmp;
+        while (std::getline(std::cin, _tmp)) {
+            if (_tmp != ";;") {
+                _commandQueue.push_back(_tmp);
+            } else {
+                break ;
+            }
+        }
+        start_work = true;
+    } else if (argc == 1) {
+        std::fstream _file(*argv);
+        if (_file.is_open()) {
+            std::string _tmp;
+            while (std::getline(_file, _tmp)) {
+                if (_tmp.compare(0, 1, ";")) {
+                    _commandQueue.push_back(_tmp);
+                }
+            }
+            start_work = true;
+        } else {
+            std::cout << "Cann't open " << *argv << "." << std::endl;
+        }
+    }
+    if (start_work) {
+        size_t i = ~0ULL;
+        while (_commandQueue.size() > ++i) {
+            std::cout << _commandQueue[i] << std::endl;
+        }
+    }
 }
